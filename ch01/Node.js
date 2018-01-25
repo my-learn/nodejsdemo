9ca1fd653744760f237eef81292da75a52450ca0
masque <masque@aliyun.com>
2018-01-25 18:41:38 +0800

    add

--- ch01/Node.js
+++ ch01/Node.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var connect = require('connect');
+
 var app = express();
-app.get('/',function (req,res) {
+app.use(connect.compress());
+app.use(express.logger());
+app.use(express.bodyParser());
+app.get('/', function (req, res) {
     res.end('hello world');
-})
+});
 app.listen(8080);
